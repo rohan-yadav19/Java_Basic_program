7ba7727dc871290d6866327acd10ffcf0a9d9c44
Rohan <yadavrohan98040@gmail.com>
2025-03-02 20:00:36 +0530

    first commit

---
+++ Add.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class Add {
+    public static void main(String[] args) {
+        int a,b,sum;
+        System.out.print("Enter two Numbers ");
+        Scanner r=new Scanner(System.in);
+        a=r.nextInt();
+        b=r.nextInt();
+      sum=a+b;
+      System.out.println("The sum of two number is " + sum);
+
+    }
+}
