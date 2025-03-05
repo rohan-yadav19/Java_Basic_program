144c3e01d76bf16bb42108d02096cd9d3bb8fc84
Rohan Yadav <yadavrohan98040@gmail.com>
2025-03-06 01:23:31 +0530

    Third commit

--- CheckVowelOrConsonent.java
+++ CheckVowelOrConsonent.java
@@ -6,8 +6,11 @@
         System.out.print("Enter any Character ");
         Scanner r=new Scanner(System.in);
         ch=r.next().charAt(0);
-        if(ch='a' ||ch='e' ||ch='i'|| ch='0'|| ch='u'){
-
+        if(ch=='a' || ch=='e' || ch=='i'|| ch=='0'|| ch=='u'){
+System.out.print("The enterd character is vowel");
+        }
+        else{
+            System.out.print("The enterd character is Consonant");
         }
     }
 }
