7699762770e41299e63bb57bf821f02394aab08f
Rohan Yadav <yadavrohan98040@gmail.com>
2025-04-16 02:09:07 +0530

    add:program to arrange array elements in asending order

---
+++ AscendingOrder.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class AscendingOrder {
+    public static void main(String[] args) {
+        int a[]=new int[5];
+        Scanner r=new Scanner(System.in);
+        System.out.print("Enter Elements in array: ");
+        for(int i=0;i<5;i++){
+            a[i]=r.nextInt();
+        }
+        for(int i=0;i<5;i++){
+            for(int j=i+1;j<5;j++){
+             if(a[i]>a[j]){
+               int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+             }
+            }
+        }
+        for(int i=0;i<5;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
